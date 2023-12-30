e5abf7ed0cb8c075b54d02acdbd6dc952c011fdf
t-powers <127955989+t-powers@users.noreply.github.com>
2023-12-29 22:08:10 -0500

    Add random number generator

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const punctuationMarks = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
 //add random index function
+function getRandom(char){
+    return Math.floor(Math.random()*char);
+}
+console.log(letters[getRandom(letters.length)])
 //call function using arrays
 //concat random array outputs
 //random sort new array
