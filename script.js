9bd3b94934bb059e14854642d8158c6a7e047484
t-powers <127955989+t-powers@users.noreply.github.com>
2023-12-30 20:35:47 -0500

    Refactoring letters

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //prettier-ignore
-const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+function randLetters(){
+  const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  return letters[getRandom(letters.length)];
+  
+}
+console.log(randLetters());
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //prettier-ignore
 const punctMarks = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-let mkPassword = document.getElementById("mkPassword");
-
 //add random index function
 function getRandom(char) {
   return Math.floor(Math.random() * char);
 }
+
 //console.log(letters[getRandom(letters.length)])
 //call function using arrays
 //concat random array outputs
