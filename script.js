f97ed26fc2babb6e06ca3a7d1086ef1bc401a9de
t-powers <127955989+t-powers@users.noreply.github.com>
2023-12-31 23:27:40 -0500

    Cleaning lines

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function randLetters(){
   const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   return letters[getRandom(letters.length)];
-  
 }
+
 function randNumbers() {
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   return numbers[getRandom(numbers.length)];
@@ -22,4 +22,5 @@ function getRandom(char) {
 
 let fieldOne = document.getElementById("passwordOne");
 let fieldTwo = document.getElementById("passwordTwo");
+
 
