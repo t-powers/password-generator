ec49b7d4e5a7ff5d8e1e4b824d0a23fc9b1053cd
t-powers <127955989+t-powers@users.noreply.github.com>
2024-01-02 21:03:12 -0500

    Removed random array functions

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 //prettier-ignore
-function randLetters(){
-  const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  return letters[getRandom(letters.length)];
-}
-console.log(randLetters());
+const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-function randNumbers() {
-  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  return numbers[getRandom(numbers.length)];
-}
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //prettier-ignore
-function randPuncts(){
-  const punctMarks = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-  return punctMarks[getRandom(punctMarks.length)];
-}
+const punctMarks = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
 //add random number function
 function getRandom(char) {
